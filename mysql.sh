78e9fcdb67ced3423a586112e5ac7d23f5d28b5f
Bronn1 <danprohorov@gmail.com>
2017-10-14 17:53:27 +0300

    fix

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,4 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE djangodb;"
-mysql -uroot -e "CREATE USER 'dj@localhost' IDENTIFIED BY 'qweasz';"
-mysql -uroot -e "GRANT ALL ON djangodb.* TO 'dj@localhost';"
+mysql -uroot -e "GRANT ALL PRIVILEGES  ON *.* TO 'root'@'localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
